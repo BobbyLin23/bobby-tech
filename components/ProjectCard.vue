5efcf7af74f9a9c0a71f4c0808a4b75c3f5c5502
BobbyLin23 <linzhangsheng23@gmail.com>
2024-08-11 23:02:09 +0800

    feat: update project card

--- components/ProjectCard.vue
+++ components/ProjectCard.vue
@@ -7,19 +7,13 @@ defineProps<{
 </script>
 
 <template>
-  <div class="grid grid-rows-3 grid-flow-col gap-4 border h-20">
-    <div class="row-span-3">
-      <img :src="image" alt="Project Image" class="w-full h-full object-cover">
-    </div>
-    <div class="col-span-2 p-2">
-      <h2 class="text-2xl font-bold">
-        {{ title }}
-      </h2>
-    </div>
-    <div class="row-span-2 col-span-2 p-2">
-      <p class="text-sm text-muted-foreground">
-        {{ description }}
-      </p>
-    </div>
-  </div>
+  <Card>
+    <CardHeader>
+      <CardTitle>{{ title }}</CardTitle>
+      <CardDescription>{{ description }}</CardDescription>
+    </CardHeader>
+    <CardContent>
+      <NuxtImg :src="image" alt="Project Image" />
+    </CardContent>
+  </Card>
 </template>
